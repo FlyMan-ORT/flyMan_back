d174b476e1235586b658127ae5a37f388515e791
dtlucho <luciano.ditomaso@gmail.com>
2022-04-27 20:05:13 -0300

    update reservations router

--- routes/reservations.js
+++ routes/reservations.js
@@ -2,18 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 //All reservations for web (WEB)
-router.get('/', async (req, res) => { });
+router.get('/', async (req, res) => {
+    res.json('All reservations');
+});
 
 //All maintenance (WEB)
-router.get('/:type', async (req, res) => { });
+router.get('/:type', async (req, res) => {
+    res.json('All reservations by type');
+});
 
 //All maintenance by id (APP)
-router.get('/:type/users/:id', async (req, res) => { });
+router.get('/:type/users/:id', async (req, res) => {
+    res.json('All reservations by type and by user');
+});
 
 //Create maintenance reservation (WEB)
-router.post('/', async (req, res) => { });
+router.post('/', async (req, res) => {
+    res.json('Create reservation');
+});
 
 //Update maintenance reservation (WEB & APP)
-router.patch('/:id', async (req, res) => { });
+router.patch('/:id', async (req, res) => {
+    res.json('Update reservation');
+});
 
 module.exports = router;
