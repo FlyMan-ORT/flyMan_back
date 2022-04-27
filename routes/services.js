7f19b25c4ed9699888925a98b53cf184b4c68df6
dtlucho <luciano.ditomaso@gmail.com>
2022-04-27 20:24:16 -0300

    services router

---
+++ routes/services.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+//Get all services
+router.get('/', async (req, res) => {
+    res.json('Get all services');
+});
+
+//Get all services
+router.get('/user/:id', async (req, res) => {
+    res.json('Get all services by user');
+});
+
+//Create services
+router.post('/', async (req, res) => {
+    res.json('Create service');
+});
+
+//Update services
+router.patch('/:id', async (req, res) => {
+    res.json('Update service');
+});
+
+//Delete services?? soft delete?? 
+router.delete('/:id', async (req, res) => {
+    res.json('Delete service');
+});
+
+module.exports = router;
