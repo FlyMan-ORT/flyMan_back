718f365234c4149c6bb4700702bc32a6d40d360e
dtlucho <luciano.ditomaso@gmail.com>
2022-04-27 20:03:40 -0300

    updating users router

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,24 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
+//Users login (APP)
+router.post('/login', async (req, res) => {
+  res.json('Login');
+});
+
+//Users register (WEB)
+router.post('/register', async (req, res) => {
+  res.json('Register');
+});
+
+//Users delete
+router.delete('/:id', async (req, res) => {
+  res.json('Delete');
+});
+
+//Users update
+router.patch('/:id', async (req, res) => {
+  res.json('Update');
+});
+
 module.exports = router;
