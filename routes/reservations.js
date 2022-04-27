e8571060466e7622e12b35783a475405df65d814
dtlucho <luciano.ditomaso@gmail.com>
2022-04-27 19:55:07 -0300

    main reservations endpoints

---
+++ routes/reservations.js
@@ -0,0 +1,17 @@
+const express = require(express);
+const router = express.Router();
+
+//All reservations for web (WEB)
+router.get('/reservations', async (req, res) => { });
+
+//All maintenance (WEB)
+router.get('/reservations/:type', async (req, res) => { });
+
+//All maintenance by id (APP)
+router.get('/reservations/:type/users/:id', async (req, res) => { });
+
+//Create maintenance reservation (WEB)
+router.post('/reservation', async (req, res) => { });
+
+//Update maintenance reservation (WEB & APP)
+router.patch('/reservation/:id', async (req, res) => { });
