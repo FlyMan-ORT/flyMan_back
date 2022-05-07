cce5763447c3468d883d88c06a5731a0ea07fa53
Leo-Fer <leonardo.fernandez.g@gmail.com>
2022-05-07 17:35:30 -0300

    GetCars Endpoint

--- routes/cars.js
+++ routes/cars.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../data/cars')
 
 //Get cars
 router.get('/', async (req, res) => {
-    res.json('Get cars');
+    let cars = await data.getAllCars();
+
+    res.json(cars);
 });
 
 //Open car
 router.post('/open', async (req, res) => {
+    
     res.json('Open car');
 });
 
