3aa6eb8359128e3e2fca2eb2e76176f0593b1f69
dtlucho <luciano.ditomaso@gmail.com>
2022-05-11 20:52:49 -0300

    Migrating endpoint logic to sevices

--- controllers/services.js
+++ controllers/services.js
@@ -1,30 +1,64 @@
 var moment = require('moment-timezone');
 const ServicesDB = require('../data/services');
 
-const saveService = async (plate, reservationId) => {
-    const service = {
-        plate: plate,
-        reservationId: reservationId,
-        userEmail: '',
-        startDate: moment.utc().tz("America/Argentina/Buenos_Aires").format(),
+const createService = async (req, res) => {
+    try {
+        const { plate, reservationId } = req.body;
+        if (!plate || !reservationId) return res.status(400).json();
+
+        const service = {
+            plate: plate,
+            reservationId: reservationId,
+            userEmail: '',
+            startDate: moment.utc().tz("America/Argentina/Buenos_Aires").format(),
+        }
+
+        const saved = await ServicesDB.saveService(service);
+        if (!saved.insertedId) return res.status(500).json();
+
+        res.status(200).json({ serviceId: saved.insertedId });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json();
     }
-
-    const result = await ServicesDB.saveService(service);
-
-    return result;
 }
 
-const getService = async (plate, reservationId) => {
-    return await ServicesDB.getService(plate, reservationId);
+const getService = async (req, res) => {
+    try {
+        const { plate, reservation } = req.params;
+        if (!plate || !reservation) return res.status(400).json();
+
+        const service = await ServicesDB.getService(plate, reservation);
+        if (!service) return res.status(404).json();
+
+        res.status(200).json({ service })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json();
+    }
 }
 
 const getServiceById = async (id) => {
     return await ServicesDB.getServiceById(id);
 }
 
-const updateService = async (id, tasks) => {
-    const endDate = moment.utc().tz("America/Argentina/Buenos_Aires").format();
-    return await ServicesDB.updateService(id, tasks, endDate);
+const updateService = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { tasks } = req.body;
+        if (!id || !tasks) return res.status(400).json();
+
+        const service = await getServiceById(id);
+        if (!service) return res.status(404).json();
+
+        const endDate = moment.utc().tz("America/Argentina/Buenos_Aires").format();
+        const updated = await ServicesDB.updateService(id, tasks, endDate);;
+        if (!updated || updated.modifiedCount === 0) return res.status(500).json();
+
+        res.status(200).json({ updated: updated.modifiedCount === 0 });
+    } catch (error) {
+        res.status(500).json();
+    }
 }
 
-module.exports = { saveService, getService, getServiceById, updateService }
+module.exports = { createService, getService, getServiceById, updateService }
--- routes/services.js
+++ routes/services.js
@@ -7,56 +7,13 @@ router.get('/', async (req, res) => {
     res.json('Get all services');
 });
 
-//Get all services NTH
-router.get('/plate/:plate/reservation/:reservation', async (req, res) => {
-    try {
-        const { plate, reservation } = req.params;
-        if (!plate || !reservation) return res.status(400).json();
-
-        const service = await ServicesController.getService(plate, reservation);
-        if (!service) return res.status(404).json();
-
-        res.status(200).json({ service })
-    } catch (error) {
-        console.log(error)
-        res.status(500).json();
-    }
-});
+router.get('/plate/:plate/reservation/:reservation', ServicesController.getService);
 
 //Create services
-router.post('/', async (req, res) => {
-    try {
-        const { plate, reservationId } = req.body;
-        if (!plate || !reservationId) return res.status(400).json();
-
-        let saved = await ServicesController.saveService(plate, reservationId);
-        if (!saved.insertedId) return res.status(500).json();
-
-        res.status(200).json({ serviceId: saved.insertedId });
-    } catch (error) {
-        console.log(error)
-        res.status(500).json();
-    }
-});
+router.post('/', ServicesController.createService);
 
 //Update services
-router.patch('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { tasks } = req.body;
-        if (!id || !tasks) return res.status(400).json();
-
-        const service = await ServicesController.getServiceById(id);
-        if (!service) return res.status(404).json();
-
-        const updated = await ServicesController.updateService(id, tasks);
-        if (!updated || updated.modifiedCount === 0) return res.status(500).json();
-
-        res.status(200).json({ updated: updated.modifiedCount === 0 });
-    } catch (error) {
-        res.status(500).json();
-    }
-});
+router.patch('/:id', ServicesController.updateService);
 
 //Delete services?? soft delete?? 
 router.delete('/:id', async (req, res) => {
