1a70df3fc4153a2ed4c6973fbf1aa42bcd160fdb
dtlucho <luciano.ditomaso@gmail.com>
2022-05-11 21:02:16 -0300

    migrating logic from endpoint to controller

---
+++ controllers/users.js
@@ -0,0 +1,63 @@
+
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const UsersDB = require('../data/users');
+
+const SALT_ROUNDS = 10;
+
+const login = async (req, res) => {
+    try {
+        let { email, password } = req.body;
+        if (!email || !password) return res.status(400).json();
+
+        // Clean fields
+        email = email.toLowerCase().trim();
+
+        // Get user
+        const user = await UsersDB.getUserByEmail(email);
+        if (!user) return res.status(404).json();
+
+        // Validate password
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) return res.status(404).json();
+
+        // replace with SECRET
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY);
+
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json();
+    }
+}
+
+const register = async (req, res) => {
+    try {
+        let { name, email, password } = req.body;
+        if (!name || !email || !password) return res.status(400).json();
+
+        // Clean fields
+        email = email.toLowerCase().trim();
+
+        const user = await UsersDB.getUserByEmail(email);
+        if (user) return res.status(400).json();
+
+        // Hash password
+        password = await bcrypt.hash(password, SALT_ROUNDS);
+
+        let saved = await UsersDB.addUser({ name, email, password });
+        if (!saved.insertedId) return res.status(500).json({ error: 'Error' });
+
+        const token = jwt.sign({ _id: saved.insertedId }, process.env.SECRET_KEY);
+
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+const getAllUsers = async (req, res) => {
+    let allUsers = await UsersDB.getAllUsers();
+    res.status(200).json(allUsers);
+}
+
+module.exports = { login, register, getAllUsers }
--- routes/users.js
+++ routes/users.js
@@ -1,68 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const users = require('../data/users');
-
-const SALT_ROUNDS = 10;
+const usersController = require('../controllers/users');
 
 /* GET users listing. */
-router.get('/', async (req, res) => {
-  let allUsers = await users.getAllUsers();
-  res.json(allUsers);
-});
+router.get('/', usersController.getAllUsers);
 
 //Users login (APP)
-router.post('/login', async (req, res) => {
-  try {
-    let { email, password } = req.body;
-    if (!email || !password) return res.status(400).json();
-
-    // Clean fields
-    email = email.toLowerCase().trim();
-
-    // Get user
-    const user = await users.getUserByEmail(email);
-    if (!user) return res.status(404).json();
-
-    // Validate password
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) return res.status(404).json();
-
-    // replace with SECRET
-    const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY);
-
-    res.status(200).json({ token });
-  } catch (error) {
-    res.status(500).json();
-  }
-});
+router.post('/login', usersController.login);
 
 //Users register (WEB)
-router.post('/register', async (req, res) => {
-  try {
-    let { name, email, password } = req.body;
-    if (!name || !email || !password) return res.status(400).json();
-
-    // Clean fields
-    email = email.toLowerCase().trim();
-
-    const user = users.getUserByEmail(email);
-    if (user) return res.status(400).json();
-
-    // Hash password
-    password = await bcrypt.hash(password, SALT_ROUNDS);
-
-    let saved = await users.addUser({ name, email, password });
-    if (!saved.insertedId) return res.status(500).json({ error: 'Error' });
-
-    const token = jwt.sign({ _id: saved.insertedId }, process.env.SECRET_KEY);
-
-    res.status(200).json(token);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
+router.post('/register', usersController.register);
 
 //Users delete
 router.delete('/:id', async (req, res) => {
