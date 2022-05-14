6a3365bfd217859e1f257ac0083bf2a6d8ef42b1
Ezequiel <ezequielmicha@gmail.com>
2022-05-14 18:12:23 -0300

    Added update and delete user functions

--- data/users.js
+++ data/users.js
@@ -1,4 +1,5 @@
 const conn = require('./connection.js');
+const { ObjectId } = require('bson');
 const DATABASE = 'FlyMan';
 const USERS_COLLECTION = 'users';
 
@@ -9,8 +10,8 @@ async function getAllUsers() {
         .collection(USERS_COLLECTION)
         .find()
         .toArray();
-
-    return users;
+//{deletedAt:{$nin:[null]}}
+    return users.filter(item => !(item.hasOwnProperty("deletedAt")));
 }
 
 async function getUserByEmail(email) {
@@ -19,6 +20,16 @@ async function getUserByEmail(email) {
         .db(DATABASE)
         .collection(USERS_COLLECTION)
         .findOne({ email: email });
+
+    return user;
+}
+
+async function getUserById(id) {
+    const connectiondb = await conn.getConnection();
+    const user = await connectiondb
+        .db(DATABASE)
+        .collection(USERS_COLLECTION)
+        .findOne({ _id: new ObjectId(id) });
 
     return user;
 }
@@ -33,4 +44,28 @@ async function addUser(user) {
     return res;
 }
 
-module.exports = { getAllUsers, addUser, getUserByEmail };
+async function updateUser(id, name, email, phone) {
+    const connectiondb = await conn.getConnection();
+    const record = await connectiondb
+        .db(DATABASE)
+        .collection(USERS_COLLECTION)
+        .updateOne(
+            { _id: new ObjectId(id) },
+            { $set: { name: name, email: email, phone: phone } }
+        );
+    return record;
+}
+
+async function deleteUser(id, deletedAt) {
+    const connectiondb = await conn.getConnection();
+    const record = await connectiondb
+        .db(DATABASE)
+        .collection(USERS_COLLECTION)
+        .updateOne(
+            { _id: new ObjectId(id) },
+            { $set: { deletedAt: deletedAt } }
+        );
+    return record;
+}
+
+module.exports = { getAllUsers, addUser, getUserByEmail, getUserById, updateUser, deleteUser };
