da5db19a901b6a0deb0c8ebacef08c275c6eafdf
dtlucho <luciano.ditomaso@gmail.com>
2022-05-25 18:01:49 -0300

    code style

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -3,17 +3,16 @@ const jwt = require('jsonwebtoken');
 
 const authorization = async (req, res, next) => {
     if (req.headers["authorization"]) {
-        let token = jwt.decode(req.headers["authorization"].replace("Bearer ", ""))
+        let token = jwt.decode(req.headers["authorization"].replace("Bearer ", ""));
         let user = await usersController.getUserById(token._id);
         if (!user) {
-            res.status(401).json({})
+            res.status(401).json({});
         } else {
             req.user = user;
             next();
         }
-
     } else {
-        res.status(401).json({})
+        res.status(401).json({});
     }
 }
 
