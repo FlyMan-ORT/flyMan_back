4d5fc021b533f242beeeb5ea6d0c6ce908a99670
dtlucho <luciano.ditomaso@gmail.com>
2022-05-25 18:14:46 -0300

    error handling

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,8 +1,12 @@
 const carsDB = require('../data/cars');
 
 const getAllCars = async (req, res) => {
-    let cars = await cars.getAllCars();
-    res.status(200).json(cars);
+    try {
+        const cars = await cars.getAllCars();
+        res.status(200).json(cars);
+    } catch (error) {
+        res.status(500).json();
+    }
 }
 
 module.exports = { getAllCars }
--- controllers/reservations.js
+++ controllers/reservations.js
@@ -1,9 +1,13 @@
 const reservationsDB = require('../data/reservations');
 
 const getAllReservationsByUser = async (req, res) => {
-    const { email } = req.user;
-    const reservations = await reservationsDB.getAllReservationsByEmail(email);
-    res.status(200).json(reservations);
+    try {
+        const { email } = req.user;
+        const reservations = await reservationsDB.getAllReservationsByEmail(email);
+        res.status(200).json(reservations);
+    } catch (error) {
+        res.status(500).json();
+    }
 }
 
 module.exports = { getAllReservationsByUser }
