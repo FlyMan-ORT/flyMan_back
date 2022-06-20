3ec251de6890e0d109b00b43b9dee9faa6d48814
ferminmorello <fermin@mubee.io>
2022-06-20 13:53:18 -0300

    Se borra console.log

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 const authorization = async (req, res, next) => {
     if (req.headers["authorization"]) {
         let token = jwt.decode(req.headers["authorization"].replace("Bearer ", ""));
-        console.log(token);
         let user = await usersController.getUserById(token._id);
         if (!user) {
             res.status(401).json({ error: "Usuario no autorizado." });
